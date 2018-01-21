b72ecfeaa4f823479cc98ef6bea05283259aeb1d
Bob Chatman <bob.chatman@gmail.com>
2018-01-21 15:22:13 -0800

    Now handling user input using window events.

--- examples/testing/main.rs
+++ examples/testing/main.rs
@@ -1,25 +1,67 @@
 extern crate rusty_playground;
 
+#[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate winit;
 
 use winit::{EventsLoop, WindowEvent as window_event, ControlFlow};
 use winit::Event::WindowEvent;
+use winit::WindowEvent::{KeyboardInput};
 
 fn main() {
+    let mut running: bool = true;
+    env_logger::init();
+
     let mut events_loop = EventsLoop::new();
 
     match rusty_playground::init(&"Testing", &events_loop) {
         Ok(_window) => {
-            events_loop.run_forever(|event| {
-                match event {
-                    WindowEvent { event: window_event::Closed, .. } => {
-                        ControlFlow::Break
-                    },
-                    _ => ControlFlow::Continue,
-                }
-            });
+            while running  {
+                events_loop.poll_events(|event| {
+                    if let WindowEvent { event, .. } = event {
+                        match event {   
+                            window_event::KeyboardInput {
+                                input: winit::KeyboardInput {
+                                    virtual_keycode: Some(winit::VirtualKeyCode::Escape),
+                                .. 
+                                },
+                                ..
+                            } | window_event::Closed => running = false,
+                            winit::WindowEvent::KeyboardInput {
+                                input,
+                                ..
+                            } => {
+                                println!("K  {:?}", event);
+                            },
+                            winit::WindowEvent::MouseInput {
+                                ..
+                            } => {
+                                println!("M  {:?}", event);
+                            },
+                            winit::WindowEvent::MouseWheel {
+                                ..
+                            } => {
+                                println!("MW {:?}", event);
+                            },
+                            winit::WindowEvent::CursorMoved {       // Sent every tick as the mouse moves within the context of this window. Only fired when the window is focused. 
+                                ..
+                            } => {}, 
+                            winit::WindowEvent::ReceivedCharacter { // Sent along with the KeyboardInput - Pressed event
+                                ..
+                            } => {}, 
+                            _ => {
+                                println!("#  {:?}", event);
+                            }
+                        }
+                    } else if let winit::Event::DeviceEvent { event, .. } = event {
+                        // TODO: DeviceEvent handled here
+                        debug!("");
+                    } else {
+                        println!("?  {:?}", event);
+                    }
+                });
+            }
         },
         Err(_error) => {
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate winit;
 
 mod util;
 
+use std::env;
 use winit::{Window, WindowBuilder, EventsLoop, CreationError};
 use util::OS;
 
@@ -11,12 +12,17 @@ pub fn get_version() -> &'static str {
     return VERSION;
 }
 
+// TODO: Replace with the string from the compile time env variable: PROFILE
+pub fn get_target() -> &'static str {
+    return "-dev";
+}
+
 pub fn init (game_name: &str, events_loop: &EventsLoop) -> Result<Window, CreationError> {
     let win_builder = WindowBuilder::new();
 
     let _window = win_builder
         .with_min_dimensions(800, 600)
-        .with_title(format!("{} v.{} :: {}", game_name, get_version(), OS::name()))
+        .with_title(format!("{} v{}{} :: {}", game_name, get_version(), get_target(), OS::name()))
         .build(&events_loop)
         .unwrap();
 
