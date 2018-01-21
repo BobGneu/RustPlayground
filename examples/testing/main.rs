6ca55a78e66ae3d69e2cd6cb332f756f428952b5
Bob Chatman <bob.chatman@gmail.com>
2018-01-21 14:22:43 -0800

    moved os references into util module

--- examples/testing/main.rs
+++ examples/testing/main.rs
@@ -4,14 +4,14 @@ extern crate log;
 extern crate env_logger;
 extern crate winit;
 
-use winit::{EventsLoop, WindowBuilder, WindowEvent as window_event, ControlFlow};
+use winit::{EventsLoop, WindowEvent as window_event, ControlFlow};
 use winit::Event::WindowEvent;
 
 fn main() {
     let mut events_loop = EventsLoop::new();
 
     match rusty_playground::init(&"Testing", &events_loop) {
-        Ok(window) => {
+        Ok(_window) => {
             events_loop.run_forever(|event| {
                 match event {
                     WindowEvent { event: window_event::Closed, .. } => {
@@ -21,7 +21,7 @@ fn main() {
                 }
             });
         },
-        Err(error) => {
+        Err(_error) => {
 
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,24 @@
 extern crate winit;
 
+mod util;
+
 use winit::{Window, WindowBuilder, EventsLoop, CreationError};
+use util::OS;
 
-// TODO: Replace this method with a version that returns the actual OS info. 
-// Should be a function that returns something like Window 10 x64 - v155551
-pub fn get_os_name() -> &'static str {
-    if cfg!(windows) {
-        return &"Windows";
-    } else if cfg!(unix) {
-        return &"Unix";
-    } else if cfg!(macos) {
-        return &"MacOS";
-    }
-
-    return &"Unknown";
-}
-
-pub fn get_os_version() -> &'static str {
-    // TODO: Find OS Version
-    return "0.0.0";
-}
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 pub fn get_version() -> &'static str { 
-    return "1.0.0";
+    return VERSION;
 }
 
 pub fn init (game_name: &str, events_loop: &EventsLoop) -> Result<Window, CreationError> {
     let win_builder = WindowBuilder::new();
 
-    let window = win_builder
+    let _window = win_builder
         .with_min_dimensions(800, 600)
-
-        // TODO: Add x64,x86 etc.
-        .with_title(format!("{} v.{} :: {}", game_name, get_version(), get_os_name()))
+        .with_title(format!("{} v.{} :: {}", game_name, get_version(), OS::name()))
         .build(&events_loop)
         .unwrap();
 
-    Ok(window)
+    Ok(_window)
 }
